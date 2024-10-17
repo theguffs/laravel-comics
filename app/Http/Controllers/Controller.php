136b3967f7af1bd2a7188841179b10c593bc6210
theguffs <youssefabdelmalak01@gmail.com>
2024-10-17 10:42:11 +0200

    migliorabile

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+class comics extends Controller
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function index(){
+        //pende l'array da config/comics.php
+        $comics = config('comics');
+        return view('comix.index',['comics'=> $comics]);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,5 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<h1>
-    Laravel Start 1
-</h1>
 
-<h2>
-    Ciao {{ $firstName }} {{ $lastName }}
-</h2>
 @endsection
